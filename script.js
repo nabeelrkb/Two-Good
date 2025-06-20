171c72040302a943a84466976b11c367194aba5d
Nabeel <azarleeban@gmail.com>
2025-06-20 23:28:48 +0500

    page 3 created

--- script.js
+++ script.js
@@ -20,3 +20,80 @@ function loadingAnimation(){
 }
 
 loadingAnimation()
+
+
+document.addEventListener('mousemove', function(dets){
+    gsap.to('#cursor', {
+        left:dets.x,
+        top:dets.y
+    })
+})
+
+
+function child1Cursor(){
+    document.querySelector('#child1').addEventListener('mouseenter', function(dets){
+     gsap.to('#cursor', {
+    transform: 'translate(-50%, -50%) scale(1)'
+     })
+    document.querySelector('#cursor').style.backgroundColor = "#FFF";
+
+})
+document.querySelector('#child1').addEventListener('mouseleave', function(dets){
+     gsap.to('#cursor', {
+    transform: 'translate(-50%, -50%) scale(0)'
+     })
+})
+}
+child1Cursor()
+
+function child2Cursor(){
+    document.querySelector('#child2').addEventListener('mouseenter', function(dets){
+     gsap.to('#cursor', {
+    transform: 'translate(-50%, -50%) scale(1)'
+
+     })
+    document.querySelector('#cursor').style.backgroundColor = "#F1EFE0";
+
+
+})
+document.querySelector('#child2').addEventListener('mouseleave', function(dets){
+     gsap.to('#cursor', {
+    transform: 'translate(-50%, -50%) scale(0)'
+
+     })
+})
+}
+child2Cursor()
+
+function child3Cursor(){
+    document.querySelector('#child3').addEventListener('mouseenter', function(dets){
+     gsap.to('#cursor', {
+    transform: 'translate(-50%, -50%) scale(1)'
+     })
+})
+document.querySelector('#child3').addEventListener('mouseleave', function(dets){
+     gsap.to('#cursor', {
+    transform: 'translate(-50%, -50%) scale(0)'
+     })
+})
+}
+child3Cursor()
+
+
+function child4Cursor(){
+    document.querySelector('#child4').addEventListener('mouseenter', function(dets){
+     gsap.to('#cursor', {
+    transform: 'translate(-50%, -50%) scale(1)'
+
+     })
+    document.querySelector('#cursor').style.backgroundColor = "#DFD1D1";
+
+})
+document.querySelector('#child4').addEventListener('mouseleave', function(dets){
+     gsap.to('#cursor', {
+    transform: 'translate(-50%, -50%) scale(0)'
+
+     })
+})
+}
+child4Cursor()
